37053753fc93ae0c35118f5e56f5ae3569c86e68
Juan Sebastian Usuga Agudelo <usuga1997@gmail.com>
2024-11-09 08:31:52 -0500

    Updates

---
+++ _next/static/chunks/app/404/page-cbb9076093f59688.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7,160,404,236,327,429],{702:function(e,n,s){Promise.resolve().then(s.bind(s,2099)),Promise.resolve().then(s.t.bind(s,2972,23))}},function(e){e.O(0,[987,99,971,117,744],function(){return e(e.s=702)}),_N_E=e.O()}]);
